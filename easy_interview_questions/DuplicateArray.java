6f1d29ff6cae0732ef5f7c8a219a67658f384ab1
krish malvia <krishmalvia@Mac.lan>
2026-02-19 14:44:49 +0530

    Remove incorrect package declarations

--- easy_interview_questions/DuplicateArray.java
+++ easy_interview_questions/DuplicateArray.java
@@ -5,7 +5,7 @@ public class DuplicateArray {
         for(int i=0;i<arr.length;i++){
             for(int j=i+1; j<arr.length; j++){
                 if(arr[i]==arr[j]){
-                    System.out.println("duplicate element " + arr[j]);
+                    System.out.println("duplicate element " + arr[i]);
                 }
             }
         }
